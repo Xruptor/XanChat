4d2eebedf2be52b282c69c74d6ae1d7b79fc9bbe
Xruptor <202515+Xruptor@users.noreply.github.com>
2020-05-12 11:34:39 -0400

    Fixed for strings that blizzard passes that are protected. Apparently Blizzard protects certain strings by sending them as a hidden texture to prevent addons from parsing the information.  Example Battle Net names.  They do this by using escape characters |K namehere |k I had to find a work around for this scenario by replacing the protected string with the BattleNet Tag.  It's not pretty but it's better than nothing.

--- XanChat.lua
+++ XanChat.lua
@@ -920,6 +920,46 @@ local function unescape(str)
     str = gsub(str, "|T.-|t", "") --textures in chat like currency coins and such
 	str = gsub(str, "|H.-|h(.-)|h", "%1") --links, just put the item description and chat color
 	str = gsub(str, "{.-}", "") --remove raid icons from chat
+	
+	--so apparently blizzard protects certain strings and returns them as textures.
+	--this causes the insert for the multiline to break and now display the line.
+	--such is the case for protected BNET Friends names and in some rare occasions names in the chat in general.
+	--These protected strings start with |K and end with |k.   Example: |K[gsf][0-9]+|k[0]+|k
+	--look under the link above for escape sequences
+	
+	if string.find(str, "|K", 1, true) then
+	
+		--str = gsub(str, "|K(.-)|k", "%1")
+		local presenceID = string.match(str, "|K(.-)|k")
+		local accountName
+		
+		local numBNet, onlineBNet = BNGetNumFriends()
+		for i = 1, numBNet do
+			local accountInfo = C_BattleNet.GetFriendAccountInfo(i)
+			--only continue if we have a account info to work with
+			if accountInfo and accountInfo.gameAccountInfo then
+				--grab the bnet name of the account, it will have |K|k in it so again it will be hidden
+				accountName = accountInfo.accountName
+				--do we even have a battle.net tag to replace it with?
+				if accountName and accountInfo.battleTag then
+					--grab the presenceID from in between the |K|k tags
+					accountName = gsub(accountName, "|K(.-)|k", "%1")
+					--if it matches the one we found earlier, then replace it with a battle.net tag instead
+					if accountName and accountName == presenceID then
+						str = gsub(str, "|K(.-)|k", accountInfo.battleTag)
+						--return out of here since we already did the replace
+						return str
+					end
+				end
+			end
+		end
+		
+		--something went wrong with replacing the name text for |K|k
+		--so lets just remove it since it will not allow text to be inserted into the multiline box, it will be empty and or hidden since |K|k returns a hidden textures
+		--for protected strings
+		str = gsub(str, "|K(.-)|k", "%1")
+	end
+
     return str
 end
 
@@ -996,6 +1036,14 @@ local function GetChatText(copyFrame, chatIndex, pageNum)
 	end
 	
 	--Debug("Chat", chatIndex, 'msgcount', msgCount, 'pages', #pages, 'start', startPos, 'end', endPos)
+	
+	--this is to debug the |K|k protected strings
+	--parentEditBox:Insert("\n--DEBUG--\n")
+	--parentEditBox:Insert("|cff00fff6To |HTest|h[Test]|h: Test1\n")
+	--parentEditBox:Insert(unescape("|cff00fff6To |HTest|h[Test]|h: Test1\n"))
+	--parentEditBox:Insert(unescape("|cff00fff6To |HBNplayer:|Kq5|k:3:271:BN_WHISPER:|Kq5|k|h[|Kq5|k]|h: testing this\n"))
+	--parentEditBox:Insert("|cff00fff6To |HBNplayer:|h[q5]|h: testing again\n")
+	--parentEditBox:Insert("|cff00fff6To |HBNplayer:TEST:3:271:BN_WHISPER:TEST|h[TEST]|h: testing last\n")
 	
 	if pageNum then
 		copyFrame.currentPage = pageNum
